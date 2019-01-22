50bd3c303bcbff385306beed791d1647013c7b1d
Hattie Samankul <hattie.samankul@hotmail.com>
2019-01-22 15:24:21 +0000

    re-organise files

---
+++ InitialProject/src/LevelOne/Main.java
@@ -0,0 +1,46 @@
+package LevelOne;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int firstNumber = 12;
+		int secondNumber = 10;
+		boolean condition = true; 
+		int loopNumber = 3; 
+		int arrayNumbers[] = {0, 1, 2, 3, 0, 5, 6, 7, 0, 9};
+		int arrayOfInts[] = new int[10];			
+		
+		String outputMessage = "";
+		
+		System.out.println("Conditionals One:");
+		outputMessage = Calculator.conditionalsOne(firstNumber, secondNumber, condition);
+		System.out.println(outputMessage + "\n");
+		
+		System.out.println("Conditionals Two:");
+		outputMessage = Calculator.conditionalsTwo(firstNumber, secondNumber);
+		System.out.println(outputMessage + "\n");
+		
+		System.out.println("Iteration:");
+		Calculator.iteration(firstNumber, secondNumber, loopNumber);	
+		System.out.println();
+		
+		System.out.println("Arrays:");
+		Calculator.arrays(arrayNumbers);	
+		System.out.println();
+		
+		System.out.println("Print Array:");
+		Calculator.printArrays(arrayNumbers);
+		System.out.println();
+		
+		System.out.println("Iteration Two:");
+		Calculator.iterationTwo(arrayOfInts);	
+		
+		/*Calculator calc = new Calculator();				
+		total = calc.subtract(firstNumber, secondNumber);		
+		outputMessage = "The subtraction of " + firstNumber + " and " + secondNumber + " is: " + total;	
+		System.out.println(outputMessage);*/
+	
+	}
+
+}
---
+++ InitialProject/src/LevelTwo/TooHot.java
@@ -0,0 +1,43 @@
+package LevelTwo;
+
+public class TooHot {
+
+	public static String isTooHot (int temperature, boolean isSummer) {
+
+		String outputMessage = "";
+		boolean tooHot = false; 
+
+		if (isSummer == true) {
+			if (temperature >= 60 && temperature <= 100) {
+				tooHot = true; 
+				outputMessage = "Too hot is: " + tooHot + " and temperature is: " + temperature;
+			}
+			else {
+				outputMessage = "Too hot is: " + tooHot + " and temperature is: " + temperature;
+			}
+		}
+		else {
+			if (temperature >= 60 && temperature <= 90) {
+				tooHot = true; 
+				outputMessage = "Too hot is: " + tooHot + " and temperature is: " + temperature;
+			}
+			else {
+				outputMessage = "Too hot is: " + tooHot + " and temperature is: " + temperature;
+			}
+		}	
+
+		return outputMessage; 
+	}
+	
+	public static void main(String[] args) {
+		
+		int temperature = 90;
+		boolean isSummer = true; 
+		
+		String outputMessage = isTooHot(temperature, isSummer);
+		
+		System.out.println(outputMessage);
+		
+	}
+
+}
