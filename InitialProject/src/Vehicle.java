bb0cc2da2bb8f463499ee3acc5f3b5e7698d345d
Hattie Samankul <hattie.samankul@hotmail.com>
2019-01-22 15:18:38 +0000

    change variables encapsulation and create some attributes for Vehicle class

---
+++ InitialProject/src/Vehicle.java
@@ -0,0 +1,14 @@
+
+public class Vehicle {
+	
+	 String registration = "";
+	 String make = "";
+	 String model = "";
+	 String colour = "";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
